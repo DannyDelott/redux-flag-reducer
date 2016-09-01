5d65e44119199b78c343b5bd6e14b81aa17f2879
Danny Delott <dannydelott@gmail.com>
2016-09-01 00:58:03 -0700

    Fix makeFlagReducer and update tests

--- src/flagReducer.js
+++ src/flagReducer.js
@@ -1,3 +1,19 @@
-export default function FlagReducer(name, onActions = [], offActions = []) {
-  return (state, action) => { };
+import intersection from 'lodash/intersection';
+
+export default function makeFlagReducer(name = 'flag', onActionTypes = [], offActionTypes = [], defaultState = false) {
+  const common = intersection(onActionTypes, offActionTypes);
+  if (common.length > 0) {
+    throw new Error(`Invalid FlagReducer ${name}: Duplicate actions found: ${JSON.stringify(common)}`);
+  }
+  return (state = defaultState, action = {}) => {
+    if (!action.type) {
+      return state;
+    }
+    if (onActionTypes.indexOf(action.type) > -1) {
+      return true;
+    }
+    if (offActionTypes.indexOf(action.type) > -1) {
+      return false;
+    }
+  };
 }
--- test/flagReducer.spec.js
+++ test/flagReducer.spec.js
@@ -1,9 +1,32 @@
-import expect from 'expect';
-import FlagReducer from 'src/flagReducer.js';
+import { expect } from 'chai';
+import makeFlagReducer from 'src/flagReducer.js';
 
-describe('FlagReducer', () => {
-  it('should return a function', () => {
-    const reducer = FlagReducer();
-    expect(reducer).toBeA('function');
+describe('makeFlagReducer', () => {
+  it('should throw an error if onActionsTypes and offActionsTypes share values', () => {
+    const proxy = () => makeFlagReducer('foo', ['FOO_ON'], ['FOO_OFF', 'FOO_ON'], false);
+    expect(proxy).to.throw(Error, 'Invalid FlagReducer foo: Duplicate actions found: ["FOO_ON"]');
+  });
+
+  it('should return a flagReducer', () => {
+    expect(makeFlagReducer).to.be.a('function');
+  });
+
+  describe('flagReducer', () => {
+    it('should return false by default', () => {
+      const reducer = makeFlagReducer('foo', ['FOO_ON'], ['FOO_OFF'], false);
+      expect(reducer()).to.equal(false);
+    });
+
+    it('should return false if an off action is given', () => {
+      const reducer = makeFlagReducer('foo', ['FOO_ON'], ['FOO_OFF'], false);
+      const action = { type: 'FOO_OFF' };
+      expect(reducer(true, action)).to.equal(false);
+    });
+
+    it('should return true if an on action is given', () => {
+      const reducer = makeFlagReducer('foo', ['FOO_ON'], ['FOO_OFF'], false);
+      const action = { type: 'FOO_ON' };
+      expect(reducer(false, action)).to.equal(true);
+    });
   });
 });
