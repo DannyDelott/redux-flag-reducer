070643441f252cd7bf33f3e02eead9cdb43621c4
Danny Delott <dannydelott@gmail.com>
2016-09-01 00:56:50 -0700

    Add chai, remove expect

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./node_modules/.bin/mocha-webpack --webpack-config ./webpack.config.test.js --recursive 'test/*.spec.js' $*
+./node_modules/.bin/mocha-webpack \
+  --webpack-config ./webpack.config.test.js \
+  --recursive \
+  -R mocha-pretty-spec-reporter \
+  'test/*.spec.js' \
+  $*
